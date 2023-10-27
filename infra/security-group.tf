d664910b84b79064eceb2589381ce02eb9ceb516
Mel <melmelissa1936@gmail.com>
2023-10-26 23:19:50 -0300

    refacts

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -3,24 +3,6 @@ resource "aws_security_group" "fiap-security-group-app" {
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "cluster_ingress_port_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
-  security_group_id = aws_security_group.fiap-security-group-app.id
-}
-
-resource "aws_security_group_rule" "cluster_ingress_port_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
-  security_group_id = aws_security_group.fiap-security-group-app.id
-}
-
 resource "aws_security_group_rule" "cluster_ingress_all" {
   type              = "ingress"
   from_port         = 0
