3c34068cf4745bdd2c269dfce90143b80d4fba3e
Melissa Teodoro <melmelissa1936@gmail.com>
2023-11-01 22:03:17 -0300

    Feature/terraform infra (#1)
    * feat: add kubernetes config
    
    * feat: refact gitignore
    
    * feat: add README.md
    
    * feat: add terraform infra
    
    * feat: add pipeline create infra
    
    * feat: add pipeline destroy.yml infra
    
    * feat: add pipeline to destroy infra
    
    * feat: create infra with terraform
    
    * refacts: adjusts secret keys
    
    * refacts: rename cluster
    
    * refacts: remove unnecessary config
    
    * refacts: create secrets with kubernetes
    
    * refacts: rename cluster
    
    * refacts
    
    * feat: create infra with terraform
    
    * refact: remove unnecessary infra container database

---
+++ infra/security-group.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "fiap-security-group-app" {
+  name        = "fiap-security-group-app"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "cluster_ingress_all" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  security_group_id = aws_security_group.fiap-security-group-app.id
+}
+
+resource "aws_security_group_rule" "cluster_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  security_group_id = aws_security_group.fiap-security-group-app.id
+}
